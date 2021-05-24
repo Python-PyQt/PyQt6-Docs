5fd723e13a44ee823bed65340e65110c287767ef
Phil Thompson <phil@riverbankcomputing.com>
2021-05-24 12:25:52 +0100

    Updated the pylupdate docs fo rthe --exclude option.

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ copyright = '{0}, Riverbank Computing Limited, The Qt Company'.format(
 # The short X.Y version.
 version = '6.1'
 # The full version, including alpha/beta/rc tags.
-release = version
+release = '6.1.1'
 
 # The number of columns in a table of class names.
 sip_class_table_columns = 4
