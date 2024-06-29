72e7be7bd60eb8eb7029888bccc6f39b60d04fea
Phil Thompson <phil@riverbankcomputing.com>
2024-06-29 10:21:02 +0100

    Updated the NEWS file.

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ copyright = '{0}, Riverbank Computing Limited, The Qt Company'.format(
 # The short X.Y version.
 version = '6.7'
 # The full version, including alpha/beta/rc tags.
-release = '6.7.0'
+release = '6.7.1'
 
 # The number of columns in a table of class names.
 sip_class_table_columns = 4
