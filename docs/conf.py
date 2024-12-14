6dcfb707a44017a24e5d7c13c342ff35f67a5b87
Phil Thompson <phil@riverbankcomputing.com>
2024-12-14 15:09:13 +0000

    Initial updates for PyQt v6.8.

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ copyright = '{0}, Riverbank Computing Limited, The Qt Company'.format(
 # |release|, also used in various other places throughout the built documents.
 #
 # The short X.Y version.
-version = '6.7'
+version = '6.8'
 # The full version, including alpha/beta/rc tags.
-release = '6.7.1'
+release = '6.8.0'
 
 # The number of columns in a table of class names.
 sip_class_table_columns = 4
